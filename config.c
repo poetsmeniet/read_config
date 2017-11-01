f23bbbaa02836685ea165838c96eda62ed3d14ea
Thomas <thomas@geenbs.nl>
2017-11-01 11:40:27 +0100

    Added zlib license

--- config.c
+++ config.c
@@ -1,8 +1,28 @@
+/*
+read_config, confguration parser
+
+Copyright (c) 2017 Thomas Wink <thomas@geenbs.nl>
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "config.h"
-//#include "logger.h"
 #define CONFIGFILE "config.txt"
 #define MAXLEN 50 
 
@@ -23,7 +43,6 @@ extern int getConfig(appConfig *config){
             size_t keyLen = strlen(sKey) + 1;
             size_t valLen = strlen(sVal) + 1;
             if(keyLen > MAXLEN || valLen > MAXLEN){
-               //logr(2, "Configuration option larger than allowed max of %d on line number %d", MAXLEN, lineNr);
                return 0;
             }
             
@@ -51,7 +70,6 @@ extern int getConfig(appConfig *config){
         }
     }else{
         //Pocess error
-        //logr(3, "Cannot parse config. No such file; '%s'", CONFIGFILE);
         return 0;
     }
     return 1;
--- config.h
+++ config.h
@@ -1,6 +1,28 @@
+/*
+read_config, confguration parser
+
+Copyright (c) 2017 Thomas Wink <thomas@geenbs.nl>
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
 #ifndef config_H_
 #define config_H_
 
+// This struct details the currently supported data types 
 typedef struct applicationConfguration{
     char serverName[200];
     int serverPort;
